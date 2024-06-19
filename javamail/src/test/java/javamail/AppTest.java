a01a0303ee65a10246cd708d7515a38fe0844f36
DiegoCaladoProgramador <diegocaladoprogramador@gmail.com>
2024-06-18 22:16:57 -0300

    criação do projeto

---
+++ javamail/src/test/java/javamail/AppTest.java
@@ -0,0 +1,18 @@
+package javamail;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+    /**
+     * Rigorous Test.
+     */
+    @Test
+    public void testApp() {
+
+    }
+}
